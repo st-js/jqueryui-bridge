2f0282dd96b5f881a45344dd7261abc6c0fffc3b
Alexandru Craciun <ax.craciun@gmail.com>
2013-08-11 19:40:17 +0200

    first version after separate artifacts

---
+++ src/main/java/org/stjs/javascript/jquery/GlobalJQueryUI.java
@@ -0,0 +1,48 @@
+/**
+ *  Copyright 2011 Alexandru Craciun, Eyal Kaspi
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.stjs.javascript.jquery;
+
+import org.stjs.javascript.annotation.GlobalScope;
+import org.stjs.javascript.annotation.SyntheticType;
+import org.stjs.javascript.dom.Element;
+import org.stjs.javascript.functions.Callback2;
+
+@GlobalScope
+@SyntheticType
+abstract public class GlobalJQueryUI extends GlobalJQuery {
+	public static GlobalJQueryUI $;
+
+	/**
+	 * jquery constructors
+	 */
+	public static <FullJQuery extends JQueryAndPlugins<?>> FullJQuery $(String path) {
+		return null;
+	}
+
+	public static <FullJQuery extends JQueryAndPlugins<?>> FullJQuery $(String path, Object context) {
+		return null;
+	}
+
+	public static <FullJQuery extends JQueryAndPlugins<?>> FullJQuery $(Object path) {
+		return null;
+	}
+
+	abstract public <FullJQuery extends JQueryAndPlugins<?>> void each(JQueryAndPlugins<FullJQuery> collection,
+			Callback2<Integer, Element> elementIterationFunction);
+
+	abstract public void widget(String name, Object plugin);
+
+}
---
+++ src/main/java/org/stjs/javascript/jquery/plugins/package-info.java
@@ -0,0 +1,3 @@
+@org.stjs.javascript.annotation.STJSBridge(sources = { "/jquery/jquery.js", "/jquery/jquery-ui.js" })
+package org.stjs.javascript.jquery.plugins;
+
